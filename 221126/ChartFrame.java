6e9cafad5efb2ba9430b617f61e329db89181244
Lee Gyu <rbcjf0219@gmail.com>
2022-11-26 12:00:52 +0900

    Update ChartFrame.java

--- 221126/ChartFrame.java
+++ 221126/ChartFrame.java
@@ -6,38 +6,16 @@ import java.awt.Graphics;
 import java.sql.SQLException;
 import java.util.Arrays;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 public class ChartFrame extends CommonFrame {
+	int[] userCount = new int[6];
 
 	public ChartFrame() {
 		super(650, 450, "지원자 분석");
 		
-		int[] userCount = new int[6];
-		
 		setLayout(new BorderLayout());
-		
-		var rs = getResulSet("SELECT\r\n"
-				+ "	COUNT(IF(year(now()) - year(u_birth) BETWEEN 10 AND 19, 1, null ) ) AS `10`,\r\n"
-				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 20 AND 29, 1, null )) AS `20`,\r\n"
-				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 30 AND 39, 1, null )) AS `30`,\r\n"
-				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 40 AND 49, 1, null )) AS `40`,\r\n"
-				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 50 AND 59, 1, null )) AS `50`\r\n"
-				+ "FROM applicant a\r\n"
-				+ "INNER JOIN user u ON a.u_no = u.u_no\r\n"
-				+ "INNER JOIN employment e ON a.e_no = e.e_no\r\n"
-				+ "WHERE e.c_no = ?", 6);
-		
-		try {
-			rs.next();
-			
-			for (int i = 1; i <= 5; i++) {
-				userCount[i] = rs.getInt(i);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
 		
 		Color[] palette = { null, Color.BLACK, Color.BLUE, Color.RED, Color.green, Color.yellow };
 		
@@ -66,13 +44,63 @@ public class ChartFrame extends CommonFrame {
 					// 50: 시작 위치
 					// (i-1) * 100: 간격을 의미
 					g.fillRect(50 + (i-1) * 100, y, 40, height);
+					g.setColor(Color.black);
+					g.drawRect(50 + (i-1) * 100, y, 40, height);
+					g.drawString( i + "0대", 60 + (i-1) * 100, 340);
 				}
 			}
 		};
 		
+		
+		var cbCompany = new JComboBox<>();
+		
+		var rs2 = getResulSet("SELECT * FROM company");
+		
+		try {
+			while (rs2.next()) {
+				cbCompany.addItem(rs2.getString("c_name"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		nPanel.add(cbCompany);
+		
+		cbCompany.addActionListener(e -> {
+			updateUserCount( cbCompany.getSelectedIndex() + 1 );
+			cPanel.repaint();
+		});
+		
+		updateUserCount(1);
+		
 		add(nPanel, "North");
 		add(cPanel);
 		
+	}
+	
+	void updateUserCount(int cNo) {
+
+		var rs = getResulSet("SELECT\r\n"
+				+ "	COUNT(IF(year(now()) - year(u_birth) BETWEEN 10 AND 19, 1, null ) ) AS `10`,\r\n"
+				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 20 AND 29, 1, null )) AS `20`,\r\n"
+				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 30 AND 39, 1, null )) AS `30`,\r\n"
+				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 40 AND 49, 1, null )) AS `40`,\r\n"
+				+ "    COUNT(IF(year(now()) - year(u_birth) BETWEEN 50 AND 59, 1, null )) AS `50`\r\n"
+				+ "FROM applicant a\r\n"
+				+ "INNER JOIN user u ON a.u_no = u.u_no\r\n"
+				+ "INNER JOIN employment e ON a.e_no = e.e_no\r\n"
+				+ "WHERE e.c_no = ?", cNo);
+		
+		try {
+			rs.next();
+			
+			for (int i = 1; i <= 5; i++) {
+				userCount[i] = rs.getInt(i);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
