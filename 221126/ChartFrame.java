3c0aecf78613bce6c24857270a8c0a1f0cdda845
Lee Gyu <rbcjf0219@gmail.com>
2022-11-26 11:43:52 +0900

    Update ChartFrame.java

--- 221126/ChartFrame.java
+++ 221126/ChartFrame.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 import javax.swing.JPanel;
 
@@ -25,7 +26,7 @@ public class ChartFrame extends CommonFrame {
 				+ "FROM applicant a\r\n"
 				+ "INNER JOIN user u ON a.u_no = u.u_no\r\n"
 				+ "INNER JOIN employment e ON a.e_no = e.e_no\r\n"
-				+ "WHERE e.c_no = ?", 1);
+				+ "WHERE e.c_no = ?", 6);
 		
 		try {
 			rs.next();
@@ -47,13 +48,26 @@ public class ChartFrame extends CommonFrame {
 			public void paint(Graphics g) {
 				super.paint(g);
 				
-				for (int i = 1; i <= 5; i++) {
-					g.setColor(palette[i]);
-					g.fillRect(i * 30, 10, 20, userCount[i] * 50);
-				}
+				// 배열의 max 값을 구하는 Stream API
+				int max = Arrays.stream(userCount).max().getAsInt();
+				int barHeight = this.getHeight() - 60;
 				
+				
+				for (int i = 1; i <= 5; i++) {
+					// int / double -> 0 or 1 (double / double 0.25)
+					// int / double 0.25 -> 0
+					double scale = (double) userCount[i] / max;
+					int height = (int) (scale * barHeight);
+					int y = barHeight - height;
+					
+					g.setColor(palette[i]);
+					
+					// 수학적인 생각
+					// 50: 시작 위치
+					// (i-1) * 100: 간격을 의미
+					g.fillRect(50 + (i-1) * 100, y, 40, height);
+				}
 			}
-			
 		};
 		
 		add(nPanel, "North");
